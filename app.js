04da910bbf26652652ebd373173d5c4323a3d522
Seyma Firat <seymagezmis@hotmail.com>
2023-06-01 15:28:01 +0100

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+//settin a variable fo kevin
+const kelvin = 100;
+//convert kelvin to celcius
+const celsius = kelvin - 273;
+//convert celcius to fahrenheit
+let fahrenheit = celsius * (9 / 5) + 32;
+//round fahrenheit
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+let newton = celsius * (33 / 100);
+newton = Math.floor(newton);
+console.log(newton);
